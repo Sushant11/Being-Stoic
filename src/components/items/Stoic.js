0ffd1771f7d2045cf0b37085920294429fddc46b
sushantchitrakar@gmail.com <sushantchitrakar@gmail.com>
2020-04-04 19:17:02 +0545

    Load

--- src/components/items/Stoic.js
+++ src/components/items/Stoic.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Card } from "antd";
 
+import { Spin } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
+
 import MARCUS from "../../images/marcus.svg";
 
 // const { Meta } = Card;
@@ -13,9 +16,18 @@ const Stoic = () => {
       className="card-attr"
       hoverable
     >
-      <img src={MARCUS} alt="Marcus" className="stoic-img"></img>
-      <p className='stoic-name'>MARCUS AURELIUS</p>
-      <p className='stoic-date'>121-180 AD</p>
+      {MARCUS ? (
+        <img src={MARCUS} alt="Marcus" className="stoic-img"></img>
+      ) : (
+        <Spin
+          indicator={
+            <LoadingOutlined style={{ fontSize: 20, color: "black" }} spin />
+          }
+        />
+      )}
+
+      <p className="stoic-name">MARCUS AURELIUS</p>
+      <p className="stoic-date">121-180 AD</p>
     </Card>
   );
 };
