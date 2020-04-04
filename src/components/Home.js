9c08c3ad60ac19c094d2f785f592e3e8ad74b6ea
sushantchitrakar@gmail.com <sushantchitrakar@gmail.com>
2020-04-04 19:01:11 +0545

    Marcus

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,87 @@
-import React from "react";
-import { Layout } from "antd";
+import React, { Fragment } from "react";
+import { Row, Col, Layout } from "antd";
 
 import Navbar from "./items/Navbar";
 
 import "../styles/App.css";
+import Stoic from "./items/Stoic";
 
-const { Header } = Layout;
+const { Header, Content } = Layout;
 
 const Home = () => {
   return (
-    <Header>
-      <Navbar/>
-    </Header>
+    <Fragment>
+      <Header>
+        <Navbar />
+      </Header>
+      <Content>
+        <Row>
+          <Col
+            xs={{ span: 24, offset: 0 }}
+            lg={{ span: 14, offset: 4 }}
+            xl={{ span: 18, offset: 3 }}
+            xxl={{ span: 12, offset: 6 }}
+          >
+            <Row>
+              <Col
+                xs={{ span: 24 }}
+                lg={{ span: 12 }}
+                md={{ span: 8 }}
+                xl={{ span: 8}}
+                xxl={{ span: 8 }}
+              >
+                <Stoic />
+              </Col>
+              <Col
+                xs={{ span: 24 }}
+                lg={{ span: 12 }}
+                md={{ span: 8 }}
+                xl={{ span: 8 }}
+                xxl={{ span: 8 }}
+              >
+                <Stoic />
+              </Col>
+              <Col
+                xs={{ span: 24 }}
+                lg={{ span: 12 }}
+                md={{ span: 8 }}
+                xl={{ span: 8 }}
+                xxl={{ span: 8 }}
+              >
+                <Stoic />
+              </Col>
+              <Col
+                xs={{ span: 24 }}
+                lg={{ span: 12 }}
+                md={{ span: 8 }}
+                xl={{ span: 8}}
+                xxl={{ span: 8 }}
+              >
+                <Stoic />
+              </Col>
+              <Col
+                xs={{ span: 24 }}
+                lg={{ span: 12 }}
+                md={{ span: 8 }}
+                xl={{ span: 8 }}
+                xxl={{ span: 8 }}
+              >
+                <Stoic />
+              </Col>
+              <Col
+                xs={{ span: 24 }}
+                lg={{ span: 12 }}
+                md={{ span: 8 }}
+                xl={{ span: 8 }}
+                xxl={{ span: 8 }}
+              >
+                <Stoic />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Content>
+    </Fragment>
   );
 };
 
---
+++ src/components/items/Stoic.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Card } from "antd";
+
+import MARCUS from "../../images/marcus.svg";
+
+// const { Meta } = Card;
+
+const Stoic = () => {
+  return (
+    <Card
+      bordered={false}
+      style={{ width: 250 }}
+      className="card-attr"
+      hoverable
+    >
+      <img src={MARCUS} alt="Marcus" className="stoic-img"></img>
+      <p className='stoic-name'>MARCUS AURELIUS</p>
+      <p>121-180 AD</p>
+    </Card>
+  );
+};
+
+export default Stoic;
